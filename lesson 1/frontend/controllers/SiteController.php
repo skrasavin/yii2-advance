d12b7bf226f466374303ac8e921a45dc9931a0c4
private <s-krasssavin@mail.ru>
2019-10-14 14:32:58 +0300

    add lesson1

--- lesson 1/frontend/controllers/SiteController.php
+++ lesson 1/frontend/controllers/SiteController.php
@@ -66,7 +66,9 @@ class SiteController extends Controller
             ],
         ];
     }
-
+    public function actionHello() {
+        return "hello world";
+    }
     /**
      * Displays homepage.
      *
